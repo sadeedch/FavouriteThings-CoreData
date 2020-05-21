492f09e5987c7419cb7a4219013e09895e509718
Sadeed Ahmad <sadeedahmadch@gmail.com>
2020-05-22 02:43:27 +1000

    can retreive the location name from coordinates

--- FavouriteThings/View/LocationView.swift
+++ FavouriteThings/View/LocationView.swift
@@ -43,20 +43,68 @@ struct LocationView: View {
                         self.longitudeString = "\(position.longitude)"
                         
                     }
-                    
+                try? self.context.save()
                 })
             }
             
+            
+            
             HStack {
-                Text("Latitude...")
-                TextField("Enter location name", text: self.$latitudeString)
+                Text("Latitude")
+                TextField("Enter location name", text: self.$latitudeString, onCommit: {
+                    guard let latitude = CLLocationDegrees(self.latitudeString),
+                          let longitude = CLLocationDegrees(self.longitudeString)
+                    else {
+                        print("Invalid Coordinates")
+                        return
+                    }
+                    
+                    self.currentPosition.latitude = latitude
+                    self.currentPosition.longitude = longitude
+                    let geoCoder = CLGeocoder()
+                    let position = CLLocation(latitude: self.currentPosition.latitude, longitude: self.currentPosition.longitude)
+                    
+                    geoCoder.reverseGeocodeLocation(position) { (placemarks, error ) in
+                        guard let placemark = placemarks?.first else {
+                            print("Error locating \(self.currentPosition.latitude) / \(self.currentPosition.longitude) / : \(error.map {"\($0)"} ?? "<unknown error>")")
+                            return
+                        }
+                        
+                        self.locationName = placemark.name ?? placemark.locality ?? placemark.subLocality ?? placemark.administrativeArea ?? placemark.country ?? "<unknown>"
+                        
+                    }
+                try? self.context.save()
+                })
             }
                
             
             
             HStack {
-                Text("Longitude...")
-                TextField("Enter location name", text: self.$longitudeString)
+                Text("Longitude")
+                TextField("Enter location name", text: self.$longitudeString, onCommit: {
+                    guard let latitude = CLLocationDegrees(self.latitudeString),
+                          let longitude = CLLocationDegrees(self.longitudeString)
+                    else {
+                        print("Invalid Coordinates")
+                        return
+                    }
+                    
+                    self.currentPosition.latitude = latitude
+                    self.currentPosition.longitude = longitude
+                    let geoCoder = CLGeocoder()
+                    let position = CLLocation(latitude: self.currentPosition.latitude, longitude: self.currentPosition.longitude)
+                    
+                    geoCoder.reverseGeocodeLocation(position) { (placemarks, error ) in
+                        guard let placemark = placemarks?.first else {
+                            print("Error locating \(self.currentPosition.latitude) / \(self.currentPosition.longitude) / : \(error.map {"\($0)"} ?? "<unknown error>")")
+                            return
+                        }
+                        
+                        self.locationName = placemark.name ?? placemark.locality ?? placemark.subLocality ?? placemark.administrativeArea ?? placemark.country ?? "<unknown>"
+                        
+                    }
+                try? self.context.save()
+                })
             }
            
             
